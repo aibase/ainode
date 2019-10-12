4c1b02d6bb325b896575055d219c93c7bb745423
stefian <stefan.ianta@w3ai.net>
2019-10-12 08:16:58 -0300

    added body-parser and createPost draft

--- controllers/feed.js
+++ controllers/feed.js
@@ -4,3 +4,12 @@ exports.getPosts = (req, res, next) => {
     });
 };
 
+exports.createPost = (req, res, next) => {
+    const title = req.body.title;
+    const content = req.body.content;
+    // Create post in db
+    res.status(201).json({
+        message: 'Post created successfully!',
+        post: {id: new Date().toISOString, title: title, content: content}
+    });
+}
