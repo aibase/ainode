1c95a2157921535edfa9791fc5f6d079ff71669c
stefian <stefan.ianta@w3ai.net>
2019-10-12 08:18:54 -0300

    // Create post in db + local storage / git repos

--- controllers/feed.js
+++ controllers/feed.js
@@ -7,7 +7,7 @@ exports.getPosts = (req, res, next) => {
 exports.createPost = (req, res, next) => {
     const title = req.body.title;
     const content = req.body.content;
-    // Create post in db
+    // Create post in db + local storage / git repos
     res.status(201).json({
         message: 'Post created successfully!',
         post: {id: new Date().toISOString, title: title, content: content}
