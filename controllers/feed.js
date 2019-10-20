56fc4b595b7df923afea91928a9ead4d2588c151
stefian <v_ianta@yahoo.com>
2019-10-20 14:07:20 -0300

    added dummy post with creator and createdAt fields

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,6 +1,17 @@
 exports.getPosts = (req, res, next) => {
     res.status(200).json({
-        posts: [{title: 'First Post', content: 'This is the first post'}]
+      posts: [
+        {
+          _id: '1',
+          title: 'First Post',
+          content: 'This is the first post!',
+          imageUrl: 'files/images/duck.jpg',
+          creator: {
+            name: 'Stefan'
+          },
+          createdAt: new Date()
+        }
+      ]
     });
 
     // Testing eval()
