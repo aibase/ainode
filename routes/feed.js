36c42d3d2cbed10efd2769659b4664edf4a61220
stefian <v_ianta@yahoo.com>
2019-10-21 07:57:10 -0300

    added express-validator rules for post title and content min: 5 chars

--- routes/feed.js
+++ routes/feed.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { body } = require('express-validator');
 
 const feedController = require('../controllers/feed');
 
@@ -7,7 +8,10 @@ const router = express.Router();
 // GET /feed/posts
 router.get('/posts', feedController.getPosts);
 
-// POST /feed/post
-router.post('/post', feedController.createPost);
+// POST /feed/post - create a Post
+router.post('/post', [
+  body('title').trim().isLength({min: 5}),
+  body('content').trim().isLength({min: 5})
+], feedController.createPost);
 
 module.exports = router;
