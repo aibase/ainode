4a0ff3067da4b1cb76d21828b397d75266dffd7e
stefian <v_ianta@yahoo.com>
2019-10-23 10:15:49 -0300

    added bcrypt pass hashed and saving to db

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const { validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 
 const User = require('../models/user');
 
@@ -13,4 +14,23 @@ exports.signup = (req, res, next) => {
   const email = req.body.email;
   const name = req.body.name;
   const password = req.body.password;
+  bcrypt
+    .hash(password, 12)
+    .then(hashedPw => {
+      const user = new User({
+        email: email,
+        password: hashedPw,
+        name: name
+      });
+      return user.save();
+    })
+    .then(result => {
+      res.status(201).json({message: 'User created!', userId: result._id})
+    })
+    .catch(err => {
+    if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+  });
 };
