b9ead4865e3bea493af6a9bfc2cfae6284dfde2b
stefian <v_ianta@yahoo.com>
2019-10-24 13:49:36 -0300

    added logic to remove the post deketed also from the user list of posts created

--- controllers/feed.js
+++ controllers/feed.js
@@ -177,7 +177,13 @@ exports.deletePost = (req, res, next) => {
       return Post.findByIdAndRemove(postId);
     })
     .then(result => {
-      console.log(result);
+      return User.findById(req.userId);
+    })
+    .then(user => {
+      user.posts.pull(postId);
+      return user.save();
+    })
+    .then(result => {
       res.status(200).json({message: 'Deleted post.'});
     })
     .catch(err => {
