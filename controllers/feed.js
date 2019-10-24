5f71927d4f752affd40189c9e24299d0a65516f7
stefian <v_ianta@yahoo.com>
2019-10-24 13:00:27 -0300

    linked posts to user/creator

--- controllers/feed.js
+++ controllers/feed.js
@@ -4,6 +4,7 @@ const path = require('path');
 const { validationResult } = require('express-validator');
 
 const Post = require('../models/post');
+const User = require('../models/user');
 
 exports.getPosts = (req, res, next) => {
   const currentPage = req.query.page || 1; // set page to 1 as a default
@@ -51,20 +52,32 @@ exports.createPost = (req, res, next) => {
   const imageUrl = req.file.path;
   const title = req.body.title;
   const content = req.body.content;
+  let creator;
   // Create post in db + local storage ~ git repos
   const post = new Post({
     title: title,
     content: content,
     imageUrl: imageUrl,
-    creator: { name: 'Stefan' }
+    creator: req.userId
   });
-  post.save().then(result => {
-    // console.log(result);
-    res.status(201).json({
-      message: 'Post created successfully!',
-      post: result
-  });
-  }).catch(err => {
+  post
+    .save()
+    .then(result => {
+      return User.findById(req.userId);
+    })
+    .then(user => {
+      creator = user;
+      user.posts.push(post);  // adding post to the list of posts of the user
+      return user.save();
+    })
+    .then(result => {
+      res.status(201).json({
+        message: 'Post created successfully!',
+        post: post,
+        creator: { _id: creator._id, name: creator.name }
+      });
+    })
+    .catch(err => {
     if (!err.statusCode) {
       err.statusCode = 500;
     }
