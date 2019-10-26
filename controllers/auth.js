57a19739076381773d8c2cef20eca5b31dcbd978
stefian <v_ianta@yahoo.com>
2019-10-26 12:32:36 -0300

    changed getUserStatus controller to Async/Await

--- controllers/auth.js
+++ controllers/auth.js
@@ -68,22 +68,21 @@ exports.login = async (req, res, next) => {
   }
 };
 
-exports.getUserStatus = (req, res, next) => {
-  User.findById(req.userId)
-    .then(user => {
-      if (!user) {
-        const error = new Error('User not found.');
-        error.statusCode = 404; // Not found error 404
-        throw error;
-      }
-      res.status(200).json({ status: user.status });
-    })
-    .catch(err => {
-      if (!err.statusCode) {
+exports.getUserStatus = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.userId)
+    if (!user) {
+      const error = new Error('User not found.');
+      error.statusCode = 404; // Not found error 404
+      throw error;
+    }
+    res.status(200).json({ status: user.status });
+  } catch (err) {
+    if (!err.statusCode) {
         err.statusCode = 500;
       }
       next(err);
-    });
+  }
 };
 
 exports.updateUserStatus = (req, res, next) => { 
