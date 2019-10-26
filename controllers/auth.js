02c186de5137458bc3a7afde736db1f4326e2386
stefian <v_ianta@yahoo.com>
2019-10-26 12:28:49 -0300

    changed login controller to Async/Await

--- controllers/auth.js
+++ controllers/auth.js
@@ -34,41 +34,38 @@ exports.signup = async (req, res, next) => {
   }
 };
 
-exports.login = (req, res, next) => { 
+exports.login = async (req, res, next) => { 
   const email = req.body.email;
   const password = req.body.password;
   let loadedUser;
-  User.findOne({ email: email })
-    .then(user => {
-      if (!user) {
-        const error = new Error('A user with this email could not be found.');
-        error.statusCode = 401; // Resource not found
-        throw error;
-      }
-      loadedUser = user;
-      return bcrypt.compare(password, user.password);
-    })
-    .then(isEqual => {
-      if (!isEqual) {
-        const error = new Error('Wrong password!');
-        error.statusCode = 401;
-        throw error;
-      }
-      // We have a valid user email and pass pair => generate JSON Web Token
-      const token = jwt.sign({
-        email: loadedUser.email,
-        userId: loadedUser._id.toString()
-      }, 'somesupersecretAINODEsecret',
-        { expiresIn: '1h' }
-      ); // if token stolen from browser it will become invalid after 1h
-      res.status(200).json({ token: token, userId: loadedUser._id.toString() });
-    })
-    .catch(err => {
-      if (!err.statusCode) {
+  try {
+    const user = await User.findOne({ email: email })
+    if (!user) {
+      const error = new Error('A user with this email could not be found.');
+      error.statusCode = 401; // Resource not found
+      throw error;
+    }
+    loadedUser = user;
+    const isEqual = await bcrypt.compare(password, user.password);
+    if (!isEqual) {
+      const error = new Error('Wrong password!');
+      error.statusCode = 401;
+      throw error;
+    }
+    // We have a valid user email and pass pair => generate JSON Web Token
+    const token = jwt.sign({
+      email: loadedUser.email,
+      userId: loadedUser._id.toString()
+    }, 'somesupersecretAINODEsecret',
+      { expiresIn: '1h' }
+    ); // if token stolen from browser it will become invalid after 1h
+    res.status(200).json({ token: token, userId: loadedUser._id.toString() });
+  } catch(err) {
+    if (!err.statusCode) {
         err.statusCode = 500;
       }
       next(err);
-    });
+  }
 };
 
 exports.getUserStatus = (req, res, next) => {
