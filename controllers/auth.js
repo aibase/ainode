9b4cc3422a43ba8a87d8193247622c8aacde6c58
stefian <v_ianta@yahoo.com>
2019-10-26 12:21:10 -0300

    changed signup controller to Async/Await

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
-exports.signup = (req, res, next) => {
+exports.signup = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     const error = new Error('Validation failed.');
@@ -15,25 +15,23 @@ exports.signup = (req, res, next) => {
   const email = req.body.email;
   const name = req.body.name;
   const password = req.body.password;
-  bcrypt
-    .hash(password, 12)
-    .then(hashedPw => {
-      const user = new User({
-        email: email,
-        password: hashedPw,
-        name: name
-      });
-      return user.save();
-    })
-    .then(result => {
-      res.status(201).json({message: 'User created!', userId: result._id})
-    })
-    .catch(err => {
-    if (!err.statusCode) {
+  try {
+    const hashedPw = await bcrypt
+      .hash(password, 12)
+
+    const user = new User({
+      email: email,
+      password: hashedPw,
+      name: name
+    });
+    const result = await user.save();
+    res.status(201).json({ message: 'User created!', userId: result._id })
+  } catch (err) {
+      if (!err.statusCode) {
         err.statusCode = 500;
       }
       next(err);
-  });
+  }
 };
 
 exports.login = (req, res, next) => { 
