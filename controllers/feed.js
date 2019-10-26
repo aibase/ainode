8a0b6d3ae274c10f17b5f515dddcfda0ff86af57
stefian <v_ianta@yahoo.com>
2019-10-26 11:28:56 -0300

    changed createPost controller to Asyn/Await

--- controllers/feed.js
+++ controllers/feed.js
@@ -30,7 +30,7 @@ exports.getPosts = async (req, res, next) => {
   }
 };
 
-exports.createPost = (req, res, next) => {
+exports.createPost = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     const error = new Error('Validation failed, entered data is incorrect.');
@@ -46,7 +46,6 @@ exports.createPost = (req, res, next) => {
   const imageUrl = req.file.path;
   const title = req.body.title;
   const content = req.body.content;
-  let creator;
   // Create post in db + local storage ~ git repos
   const post = new Post({
     title: title,
@@ -54,29 +53,22 @@ exports.createPost = (req, res, next) => {
     imageUrl: imageUrl,
     creator: req.userId
   });
-  post
-    .save()
-    .then(result => {
-      return User.findById(req.userId);
-    })
-    .then(user => {
-      creator = user;
-      user.posts.push(post);  // adding post to the list of posts of the user
-      return user.save();
-    })
-    .then(result => {
-      res.status(201).json({
-        message: 'Post created successfully!',
-        post: post,
-        creator: { _id: creator._id, name: creator.name }
-      });
-    })
-    .catch(err => {
+  try {
+    await post.save();
+    const user = await User.findById(req.userId);
+    user.posts.push(post);  // adding post to the list of posts of the user
+    await user.save();
+    res.status(201).json({
+      message: 'Post created successfully!',
+      post: post,
+      creator: { _id: user._id, name: user.name }
+    });
+  } catch (err) {
     if (!err.statusCode) {
       err.statusCode = 500;
     }
     next(err);  // passing the err to error handler middleware
-  });
+  }
 };
 
 exports.getPost = (req, res, next) => { 
