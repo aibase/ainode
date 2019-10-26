41dc5cd414a71fcb75c9cd5b8520e1f280dacfd6
stefian <v_ianta@yahoo.com>
2019-10-26 12:06:07 -0300

    changed updatePost controller to Async/Await

--- controllers/feed.js
+++ controllers/feed.js
@@ -92,7 +92,7 @@ exports.getPost = async (req, res, next) => {
   }
 };
 
-exports.updatePost = (req, res, next) => {
+exports.updatePost = async (req, res, next) => {
   const postId = req.params.postId;
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
@@ -112,35 +112,32 @@ exports.updatePost = (req, res, next) => {
     throw error;
   }
   // Update data in the database
-  Post.findById(postId)
-    .then(post => {
-      if (!post) {
-        const error = new Error('Could not find post.');
-        error.statuscode = 404; // 404 - not found code
-        throw error;
-      }
-      if (post.creator.toString() !== req.userId) {
-        const error = new Error('Not authorized!')
-        error.statusCode = 403;
-        throw error;
-      }
-      if (imageUrl !== post.imageUrl) {
-        clearImage(post.imageUrl);
-      }
-      post.title = title;
-      post.imageUrl = imageUrl;
-      post.content = content;
-      return post.save();
-    })
-    .then(result => {
-      res.status(200).json({message: 'Post updated!', post: result});
-    })
-    .catch(err => {
-      if (!err.statusCode) {
+  try {
+    const post = await Post.findById(postId)
+    if (!post) {
+      const error = new Error('Could not find post.');
+      error.statuscode = 404; // 404 - not found code
+      throw error;
+    }
+    if (post.creator.toString() !== req.userId) {
+      const error = new Error('Not authorized!')
+      error.statusCode = 403;
+      throw error;
+    }
+    if (imageUrl !== post.imageUrl) {
+      clearImage(post.imageUrl);
+    }
+    post.title = title;
+    post.imageUrl = imageUrl;
+    post.content = content;
+    const result = await post.save();
+    res.status(200).json({ message: 'Post updated!', post: result });
+  } catch (err) {
+    if (!err.statusCode) {
         err.statusCode = 500;
       }
       next(err);  // passing the err to error handler middleware
-    })
+  }
 };
 
 exports.deletePost = (req, res, next) => {
