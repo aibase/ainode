53f8a84b0637dee1616fb0291f6b1db521391fd4
stefian <v_ianta@yahoo.com>
2019-10-27 10:59:06 -0300

    added emit event for channel posts on the deletePost controller method

--- controllers/feed.js
+++ controllers/feed.js
@@ -167,6 +167,7 @@ exports.deletePost = async (req, res, next) => {
     const user = await User.findById(req.userId);
     user.posts.pull(postId);
     await user.save();
+    io.getIO().emit('posts', { action: 'delete', post: postId })
     res.status(200).json({ message: 'Deleted post.' });
   } catch (err) {
     if (!err.statusCode) {
