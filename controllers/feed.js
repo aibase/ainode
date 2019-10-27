b97cd62678d9217edec1f5bb60a0f6eeafba95e3
stefian <v_ianta@yahoo.com>
2019-10-27 10:53:19 -0300

    added sorting to get latest posts on top

--- controllers/feed.js
+++ controllers/feed.js
@@ -15,6 +15,7 @@ exports.getPosts = async (req, res, next) => {
     const totalItems = await Post.find().countDocuments();
     const posts = await Post.find()
       .populate('creator')
+      .sort({ createdAt: -1 })
       .skip((currentPage - 1) * perPage)
       .limit(perPage);
 
